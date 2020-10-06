75ce4101c449256c8b80febb5b289b82e3e78b68
Roman Zelinskiy <zelinskiyrk@gmail.com>
2020-10-06 17:14:26 +0400

    Task006 - Getter, Setter (v2)

--- console/src/main/java/com/zelinskiyrk/maven/console/App.java
+++ console/src/main/java/com/zelinskiyrk/maven/console/App.java
@@ -33,5 +33,11 @@ public class App {
         System.out.println("-------= Task 5 =-------");
         Task005.CleanupExample.run();
 
+        System.out.println("-------= Task 6 =-------");
+        Task006.GetterSetterExample gSExample = new Task006.GetterSetterExample();
+        System.out.println(gSExample.getAge());
+        gSExample.setName("Vasiliy");
+        System.out.println(gSExample);
+
     }
 }
