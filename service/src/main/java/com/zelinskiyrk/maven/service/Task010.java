e1cfb65dc22feabda63cbe92942e5a1d3d78c91e
Prokurator <fiordi@yandex.ru>
2020-10-15 16:42:32 +0400

    Task10 - Data annotation

--- service/src/main/java/com/zelinskiyrk/maven/service/Task010.java
+++ service/src/main/java/com/zelinskiyrk/maven/service/Task010.java
@@ -1,4 +1,20 @@
 package com.zelinskiyrk.maven.service;
 
-public class Task010 {
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data public class Task010 {
+    private final String name;
+    @Setter (AccessLevel.PACKAGE) private int age;
+    private double score;
+    private String[] tags;
+
+    @ToString(includeFieldNames = true)
+    @Data(staticConstructor = "of")
+    public static class Exercise<T> {
+        private final String name;
+        private final T value;
+    }
 }
