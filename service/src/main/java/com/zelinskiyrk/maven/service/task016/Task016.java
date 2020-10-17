a344cd427b2ddd9f88e8e4f4a01572e6fa38025e
Prokurator <fiordi@yandex.ru>
2020-10-17 22:14:45 +0400

    Task16 - Log annotation

---
+++ service/src/main/java/com/zelinskiyrk/maven/service/task016/Task016.java
@@ -0,0 +1,13 @@
+package com.zelinskiyrk.maven.service.task016;
+
+import lombok.extern.apachecommons.CommonsLog;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
+@Log
+public class Task016 {
+    public static void logFunctions() {
+        log.severe("Something's wrong here");
+    }
+}
+
